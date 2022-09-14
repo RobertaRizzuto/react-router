4add93ffb909ad89b8ce72c25d9ca55b49087f80
RobertaRizzuto <rtarizzuto@gmail.com>
2022-09-14 19:49:59 +0200

    chore:recipe instructions

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -3,57 +3,34 @@ import styles from "./index.module.scss";
 import useFetch from "../../utils/api/use-fetch";
 import ENDPOINTS from "../../utils/api/endpoints";
 
-const formatRecipe = (data) => {
-  const initialRecipe = data.meals?.at(0) ?? {};
-  console.log("data", data);
-  const ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    const name = initialRecipe[`strIngredient${i}`];
-    const value = initialRecipe[`strMeasure${i}`];
+const Recipe = () => {
+    const { categoryName, recipeName, id } = useParams();
+  
+    const { data, loading, error } = useFetch(
+      `${ENDPOINTS.DETAIL}?i=${id}`
+    );
+    const initialRecipe = data?.meals?.at(0) ?? {};
+    console.log(initialRecipe)
+    return (
+      <div className={styles.Recipe}>
+        <h1>
+          {categoryName} -{recipeName}
+        </h1>
+      <p>{initialRecipe?.strInstructions}</p>
+        <ul>
+          <li>Istruzioni</li>
+          <li>Ingredienti</li>
+          <li>YouTube</li>
+        </ul>
+  
+        <Outlet context={data} />
+  
+        {/* {tab === "youtube" && <p>{initialRecipe?.youtubeUrl}</p>}
+        {tab === "instructions" && <p>{initialRecipe?.instructions}</p>} */}
+      </div>
+    );
 
-    if (!name?.length) {
-      continue;
+
     }
 
-    ingredients.push({ name, value });
-  }
-  return {
-    name: initialRecipe.strMeal,
-    id: initialRecipe.idMeal,
-    youtubeUrl: initialRecipe.strYoutube,
-    thumbnailSrc: initialRecipe.strMealThumb,
-    tags: initialRecipe.strTags,
-    instructions: initialRecipe.strInstructions,
-    ingredients,
-  };
-};
-
-const Recipe = (props) => {
-  const { categoryName, recipeName, id } = useParams();
-//   const { tab } = props;
-  const { data, loading, error } = useFetch(
-    `${ENDPOINTS.DETAIL}?i=${id}`,
-    formatRecipe
-  );
-
-  return (
-    <div className={styles.Recipe}>
-      <h1>
-        {categoryName} -{recipeName}
-      </h1>
-
-      <ul>
-        <li>Istruzioni</li>
-        <li>Ingredienti</li>
-        <li>YouTube</li>
-      </ul>
-
-      <Outlet context={data} />
-{/* 
-      {tab === "youtube" && <p>{data.youtubeUrl}</p>}
-      {tab === "instructions" && <p>{data.instructions}</p>} */}
-    </div>
-  );
-};
-
-export default Recipe;
+    export default Recipe
